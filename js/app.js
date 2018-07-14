edce133a69cc6f5763651f9bfaf0351072d431d7
Carmen-Wellborn <carmenwellborn@gmail.com>
2018-07-13 21:22:49 -0600

    Set up eventListener with a displayCard function

--- js/app.js
+++ js/app.js
@@ -26,13 +26,26 @@ function shuffle(array) {
 }
 
 
+const card = document.querySelector('.card');
+
+card.addEventListener('click', function(){
+displayCard();
+
+});
+
+function displayCard (){
+  card.classList.toggle('open');
+  card.classList.toggle('show');
+}
+
+
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * 1- set up the event listener for a card. If a card is clicked:
+ * 1.1 - display the card's symbol (put this functionality in another function that you call from this one)
+ * 1.2 - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ * 1.3 - if the list already has another card, check to see if the two cards match
+ * 2   + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ * 3   + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ * 4   + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ * 5   + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
