464caffcc3bdf3327585c753ff08e375c2290098
Carmen-Wellborn <carmenwellborn@gmail.com>
2018-07-14 22:04:28 -0600

    Create array that holds class of cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-
+var classesOfCards = ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb',
+                      'fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb'];
 
 /*
  * Display the cards on the page
@@ -25,11 +26,11 @@ function shuffle(array) {
     return array;
 }
 
-
 const card = document.querySelector('.card');
 
 card.addEventListener('click', function(){
-displayCard();
+
+  displayCard();
 
 });
 
