769fcaa208e83d64d577ab03127339935561224e
Carmen-Wellborn <carmenwellborn@gmail.com>
2018-07-25 14:54:53 -0400

    Obtain the number of stars

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-/*
- * Create a list that holds all of your cards
-*/
+// Create a list that holds all of your cards
 const classesOfCards = ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb',
                       'fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb'];
 
@@ -19,21 +17,21 @@ function shuffle(array) {
     return array;
 }
 
+// Every 10 moves, remove a star
 function removeStars() {
   var star = document.querySelector('.stars li');
   star.parentNode.removeChild(star);
 }
 
 
-
-//* Display the cards on the page
+// Display the cards on the page
 function cardsInDeck () {
-  //- shuffle the list of cards using the provided "shuffle" method below
+  // shuffle the list of cards using the provided "shuffle" method below
   shuffle(classesOfCards);
   const deck = document.querySelector('ul.deck');
 
+  // loop through each card and create its HTML
   for (let i=0; i<=15; i++){
-    //- loop through each card and create its HTML
     const icon = classesOfCards[i];
     const listOfCard = document.createElement('li');
     listOfCard.classList.add('card');
@@ -42,7 +40,7 @@ function cardsInDeck () {
   const iElement = document.createElement('i');
   iElement.classList.add('fa',icon);
 
-  //- add each card's HTML to the page
+  // add each card's HTML to the page
   listOfCard.appendChild(iElement);
   }
 
@@ -50,7 +48,7 @@ function cardsInDeck () {
 
 cardsInDeck();
 
-// - If a card is clicked: display the card's symbol
+// If a card is clicked: display the card's symbol
 function displayCard (card){
    card.classList.add('open', 'show');
 }
@@ -71,13 +69,13 @@ function stopTimer (){
 }
 
 function modalAnswer() {
-     //If user press OK reload the page
-    if (confirm("You WON the game!! \n\n Time: " + time + " seconds \n Moves: " + totalCountMoves + "\n\n Press 'OK' to play again")) {
+     // If user press OK reloads the pages
+    if (confirm("You WON the game!! \n\n Time: " + time + " seconds \n Moves: " + totalCountMoves + "\n Stars: " + stars + "\n\n Do you want to play again? Press 'OK'")) {
         reloadPage();
     }
 }
 
-//* - add the card to a *list* of "open" cards
+// Add the card to a *list* of "open" cards
 let listOfCards=[];
 let countMatch = 0;
 function doMatch () {
@@ -90,26 +88,30 @@ function doMatch () {
   listOfCards[0].classList.remove('open','show');
   listOfCards[1].classList.remove('open','show');
 
-  if (countMatch===8) {
-    stopTimer();
-
+  if (countMatch===1) {
     listOfCards[0].classList.remove('animation');
     listOfCards[1].classList.remove('animation');
 
-    modalAnswer();
+    stopTimer();
+    setTimeout(function(){
+      modalAnswer();
+    }, 1000);
+
   }
 
 }
 
+// Remove style when the cards dont match
 function doNoMatch() {
   listOfCards[0].classList.remove('open','show');
   listOfCards[1].classList.remove('open','show');
 }
 
+// Functionality when a card is open
 function listOfOpenCards(card){
 
   listOfCards.push(card);
-  //*- if the list already has another card, check to see if the two cards match
+  // if the list already has another card, check to see if the two cards match
 
   if (listOfCards.length===2){
     const card1 = listOfCards[0].innerHTML;
@@ -121,12 +123,12 @@ function listOfOpenCards(card){
 
     setTimeout(function(){
 
-      //if the cards do match, lock the cards in the open position
+      // if the cards do match, lock the cards in the open position
       if (card1===card2) {
         doMatch();
       }
 
-      //if the cards do not match, remove the cards from the list and hide the card's symbol
+      // if the cards do not match, remove the cards from the list and hide the card's symbol
       else if (card1!=card2) {
         doNoMatch();
           }
@@ -139,8 +141,8 @@ function listOfOpenCards(card){
 
 }
 
-let totalCountMoves;
 //Increment the move counter, every two cards
+let totalCountMoves;
 function countMoves(count){
   totalCountMoves = count/2;
   const moves = document.querySelector('.moves');
@@ -151,17 +153,19 @@ function countMoves(count){
 
 // Count the number of clicks
 let count = 0;
+// Count the number of stars
+let stars = 3;
 //* set up the event listener for a card.
 const deck = document.querySelector('.deck');
 deck.addEventListener('click', function (evt) {
   const card = evt.target;
   if (card.nodeName === 'LI') {
     count++;
-    //Start the timer on the first click
+    // Start the timer on the first click
     if (count===1) {
         startTimer();
     }
-    //Conditional to prevent to click a third card
+    // Conditional to prevent to click a third card
     if (listOfCards.length<2) {
       displayCard(card);
 
@@ -172,6 +176,15 @@ deck.addEventListener('click', function (evt) {
       countStars = count / 2;
       if ((countStars === 10) || (countStars === 20) || (countStars === 30)) {
         removeStars();
+        if ((countStars >=10) && (countStars<=19)){
+          stars = 2;
+        }
+        else if ((countStars>=20) && (countStars <=29)) {
+          stars = 1;
+        }
+        else if (countStars>=30) {
+          stars = 0;
+        }
       }
 
     }
@@ -179,11 +192,12 @@ deck.addEventListener('click', function (evt) {
   }
 });
 
-//Restart the page
+// Restart the page
 function reloadPage() {
     location.reload();
 }
 
+// Listener of button restart
 const reload = document.querySelector('.restart');
 reload.addEventListener('click', function () {
   reloadPage();
